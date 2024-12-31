1a8e0cb7f37a66e4f02aec051b020a75c12e5b55
Benno Daenen <benno.daenen@gmail.com>
2024-12-31 16:14:15 +0100

    Shuffled around some levels to improve gameflow

--- scenes/objects/powerup_link.gd
+++ scenes/objects/powerup_link.gd
@@ -5,11 +5,8 @@ func _ready() -> void:
     if (Globals.player_skills.can_link):
         queue_free()
 
-
-
 func _on_picked_up(body: Node2D) -> void:
      # Technically this isn't necessary as we have a collision mask on the player group already, but it's a good Group example
-    if (body.is_in_group('Player')):
+    if (body.is_in_group('Player') and !Globals.player_skills.can_link):
         body.set_can_link(true)
         $AudioStreamPlayer.play()
-
