e71f8426189675c1bba14c8f160a770c0f06d8af
Benno Daenen <benno.daenen@gmail.com>
2025-01-17 11:31:18 +0100

    Added screen-shake and sfx when collapsing pyramid's top floor.

--- autoload/screen_shaker.gd
+++ autoload/screen_shaker.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var rand: RandomNumberGenerator = RandomNumberGenerator.new()
+signal shake_end()
 
 @export var SHAKE_STRENGTH: float = 2
 @export var camera: Camera2D
@@ -10,11 +11,15 @@ var current_i: int = 0
 func _ready() -> void:
     rand.randomize()
 
-func shake(duration: int) -> void:
+func shake(duration: int, location: Vector2 = Vector2.ZERO) -> void:
     for i in range(0, 50):
         shake_pattern.append(Vector2(rand.randf_range(-SHAKE_STRENGTH, SHAKE_STRENGTH), rand.randf_range(-SHAKE_STRENGTH, SHAKE_STRENGTH)))
     $ShakeDuration.wait_time = duration
+    $AudioStreamPlayer2D.position = location
+    $AudioStreamPlayer2D2.position = location
+    $AudioStreamPlayer2D.play()
     $ShakeDuration.start()
+    $AudioStream2Delay.start()
 
 func _process(_delta: float) -> void:
     if shake_pattern.size():
@@ -25,3 +30,10 @@ func _on_shake_duration_timeout() -> void:
     camera.offset = Vector2(0, 0)
     current_i = 0
     shake_pattern = []
+    $AudioStreamPlayer2D.stop()
+    $AudioStreamPlayer2D2.stop()
+    shake_end.emit()
+
+
+func _on_audio_stream_2_delay_timeout() -> void:
+    $AudioStreamPlayer2D2.play()
--- scenes/levels/level_4_2.gd
+++ scenes/levels/level_4_2.gd
@@ -33,4 +33,10 @@ func _ready() -> void:
             else:
                 child.connect('switch_press', func update_progress() -> void:
                     Globals.progress_flags.right_button_pressed = true
+                    if Globals.progress_flags.left_button_pressed:
+                        $ScreenShaker.shake(3.21, Vector2(-840, 0))
+                        %Player.input_disabled = true
+                        $ScreenShaker.connect('shake_end', func reactivate_player() -> void:
+                            %Player.input_disabled = false
+                        , CONNECT_ONE_SHOT)
                 )
